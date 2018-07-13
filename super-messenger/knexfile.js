14823954f3d2e89202db544d5ea9741e3ff6768f
Ryder Dale <rydersdale@gmail.com>
2018-07-13 10:19:13 -0500

    deploying to heroku

--- super-messenger/knexfile.js
+++ super-messenger/knexfile.js
@@ -8,6 +8,6 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL
+    connection: 'postgres://fwiamkqaghseau:f0462177274209aade871aa476eb1984232c3e97ef6b4519dbde4c22a3b3a89b@ec2-107-22-169-45.compute-1.amazonaws.com:5432/d1l145i8dgm6op'
     }
 };
